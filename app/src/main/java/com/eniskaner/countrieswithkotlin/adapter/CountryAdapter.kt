d5f9ba8df8c8670023d23921f917f5cf1484c17b
Enis <enis.d.kaner@gmail.com>
2023-06-06 23:24:00 +0300

    viewbinding added on CountryAdapter

--- app/src/main/java/com/eniskaner/countrieswithkotlin/adapter/CountryAdapter.kt
+++ app/src/main/java/com/eniskaner/countrieswithkotlin/adapter/CountryAdapter.kt
@@ -4,31 +4,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.eniskaner.countrieswithkotlin.R
+import com.eniskaner.countrieswithkotlin.databinding.ItemCountryBinding
 import com.eniskaner.countrieswithkotlin.model.Country
 import com.eniskaner.countrieswithkotlin.view.FeedFragmentDirections
-
 class CountryAdapter(val countryList: ArrayList<Country>): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
-    class CountryViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+    class CountryViewHolder(val binding: ItemCountryBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+    private val differCallback = object : DiffUtil.ItemCallback<Country>(){
+        override fun areItemsTheSame(oldItem: Country, newItem: Country): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Country, newItem: Country): Boolean {
+            return oldItem == newItem
+        }
 
     }
 
+    val differ = AsyncListDiffer(this, differCallback)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_country, parent, false)
-        return CountryViewHolder(view)
+        val binding = ItemCountryBinding.inflate(LayoutInflater.from(parent.context))
+        return CountryViewHolder(binding)
+    }
+
+    private var onItemClickListener:((Country) -> Unit)? = null
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        val country = differ.currentList[position]
+        holder.binding.countryName.text = countryList[position].countryName
+        holder.binding.countryRegion.text = countryList[position].countryRegion
+
+        holder.itemView.apply {
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(country)
+                }
+            }
+        }
+
+    }
+
+    fun setOnItemClickListener(listener: (Country) -> Unit) {
+        onItemClickListener = listener
     }
 
     override fun getItemCount(): Int {
         return countryList.size
-    }
-
-    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        holder.view.setOnClickListener {
-            val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
     }
     fun updateCountryList(newCountryList: List<Country>) {
         countryList.clear()
