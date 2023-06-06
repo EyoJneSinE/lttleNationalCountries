4624349e7fb0d4659149cc76c90933a2b96702e0
Enis <enis.d.kaner@gmail.com>
2023-06-06 15:29:46 +0300

    packages added for MVVM

--- app/src/main/java/com/eniskaner/countrieswithkotlin/view/MainActivity.kt
+++ app/src/main/java/com/eniskaner/countrieswithkotlin/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.eniskaner.countrieswithkotlin
+package com.eniskaner.countrieswithkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eniskaner.countrieswithkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
