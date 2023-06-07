5c7829a4959e8a78d8eb980e1e96acf64530b211
Enis <enis.d.kaner@gmail.com>
2023-06-07 18:35:46 +0300

    data moved with coroutine in roomdb on FeedViewModel

--- app/src/main/java/com/eniskaner/countrieswithkotlin/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/eniskaner/countrieswithkotlin/viewmodel/FeedViewModel.kt
@@ -1,16 +1,18 @@
 package com.eniskaner.countrieswithkotlin.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.eniskaner.countrieswithkotlin.model.Country
 import com.eniskaner.countrieswithkotlin.service.CountryAPIService
+import com.eniskaner.countrieswithkotlin.service.CountryDatabase
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
 
-class FeedViewModel: ViewModel() {
+class FeedViewModel(application: Application) : BaseViewModel(application) {
 
     private val countryApiService = CountryAPIService()
     private val disposable = CompositeDisposable()
@@ -32,9 +34,7 @@ class FeedViewModel: ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<Country>>(){
                     override fun onSuccess(t: List<Country>) {
-                        countries.value = t
-                        countryError.value = false
-                        countryLoading.value = false
+                        storeInSQLite(t)
                     }
 
                     override fun onError(e: Throwable) {
@@ -45,5 +45,25 @@ class FeedViewModel: ViewModel() {
 
                 })
         )
+    }
+    private fun showCountries(countryList: List<Country>) {
+        countries.value = countryList
+        countryError.value = false
+        countryLoading.value = false
+    }
+
+    private fun storeInSQLite(list: List<Country>) {
+        launch {
+            val dao = CountryDatabase(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            val listLong = dao.insertAll(*list.toTypedArray()) // -> list -> individual
+            var i = 0
+            while (i < list.size) {
+                list[i].uuid = listLong[i].toInt()
+                i = i + 1
+            }
+
+            showCountries(list)
+        }
     }
 }
