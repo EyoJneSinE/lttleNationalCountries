ae8fa9adccc05fedc9671c26ee10f5b8db52dcc5
Enis <enis.d.kaner@gmail.com>
2023-06-08 23:58:06 +0300

    CountrFragment contents changed for databinding

--- app/src/main/java/com/eniskaner/countrieswithkotlin/view/CountryFragment.kt
+++ app/src/main/java/com/eniskaner/countrieswithkotlin/view/CountryFragment.kt
@@ -1,48 +1,47 @@
 package com.eniskaner.countrieswithkotlin.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.eniskaner.countrieswithkotlin.base.BaseFragment
+import com.eniskaner.countrieswithkotlin.R
 import com.eniskaner.countrieswithkotlin.databinding.FragmentCountryBinding
-import com.eniskaner.countrieswithkotlin.util.downloadFromUrl
-import com.eniskaner.countrieswithkotlin.util.placeholderProgressBar
 import com.eniskaner.countrieswithkotlin.viewmodel.CountryViewModel
-class CountryFragment : BaseFragment<FragmentCountryBinding>() {
-
-    override fun setBinding(): FragmentCountryBinding = FragmentCountryBinding.inflate(layoutInflater)
+class CountryFragment : Fragment() {
 
     private lateinit var viewModel : CountryViewModel
     private var countryUuid = 0
-
+    private lateinit var dataBinding : FragmentCountryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_country,container,false)
+        return dataBinding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         arguments?.let {
             countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
         }
-
         viewModel = ViewModelProviders.of(this).get(CountryViewModel::class.java)
         viewModel.getDataFromRoom(countryUuid)
 
         observeLiveData()
     }
-
     private fun observeLiveData() {
-        viewModel.countryLiveData.observe(viewLifecycleOwner, Observer { country ->
+        viewModel.countryLiveData.observe(viewLifecycleOwner, Observer { country->
             country?.let {
-                binding.apply {
-                    countryListName.text = country.countryName
-                    countryListCapital.text = country.countryCapital
-                    countryListCurrency.text = country.countryCurrency
-                    countryListLanguage.text = country.countryLanguage
-                    countryListRegion.text = country.countryRegion
-
-                    context?.let {
-                        countryListImage.downloadFromUrl(country.imageUrl, placeholderProgressBar(it))
-                    }
-                }
+                dataBinding.selectedCountry = country
             }
         })
     }
